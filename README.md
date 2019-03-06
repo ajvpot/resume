5f8dc2504a8eb9a0c1dff9d6d79941ed1d7f7b6b
Alex Vanderpot <alex@vanderpot.com>
2019-03-06 15:37:56 -0500

    Refactor: Move items into SectionItem, change sections into function components

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Alex Vanderpot's Résumé
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
---
+++ src/resume/util/sectionItem.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import withStyles from "@material-ui/core/styles/withStyles";
+import Typography from "@material-ui/core/Typography/Typography";
+import Chip from '@material-ui/core/Chip';
+
+const styles = (theme) => ({
+    row: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    jobHeader: {
+        fontWeight: '500',
+    },
+    chip: {
+        marginRight: theme.spacing.unit / 2,
+    }
+});
+
+class SectionItem extends Component {
+    render() {
+        const { classes, heading, time, chips, description } = this.props;
+        return (
+            <React.Fragment>
+                <div className={classes.row}>
+                    <Typography
+                        variant="subheading"
+                        style={{
+                            flexGrow: 1,
+                        }}
+                        className={classes.jobHeader}
+                    >
+                        {heading}
+                    </Typography>
+                    {time &&
+                        <Typography
+                            variant="subheading"
+                        >
+                            {time}
+                        </Typography>
+                    }
+                </div>
+                {chips && chips.map((chip) => (
+                    <Chip
+                        {...chip}
+                        className={classes.chip}
+                        variant="outlined"
+                    />
+                ))}
+                <Typography variant="body1">
+                    {description}
+                </Typography>
+            </React.Fragment>
+        );
+    }
+}
+
+export default withStyles(styles)(SectionItem);
