bb18f786aa787583b74859e2a5cae04b8fab92de
alex <alex@vanderpot.com>
2019-05-26 16:32:48 +0000

    Update Experience.js

--- src/resume/Experience.js
+++ src/resume/Experience.js
@@ -8,25 +8,27 @@ const experienceData = [
         time: '2018 - 2019',
         chips: [
             {
-                label: 'React',
+                label: 'GitLab CI',
                 color: 'primary',
             },
-            { 
-                label: 'Redux',
+            {
+                label: 'Kubernetes',
                 color: 'primary',
             },
-            { label: 'Node.js' },
-            { label: 'GitLab CI' },
-            { label: 'Kubernetes' },
             { label: 'Helm' },
+            { label: 'React' },
+            { label: 'Redux' },
+            { label: 'Node.js' },
         ],
         description: <ul style={{ margin: 0, paddingLeft: 20 }}>
-            <li>Created React components to be consumed by a suite of applications.</li>
-            <li>Created automatically generated documentation for component catalogs and a self-generating React.js Icon package.</li>
-            <li>Maintained a fork of create-react-app to automate bootstrapping of new projects.</li>
-            <li>Created CI/CD pipelines for frontend (Angular/React) and backend (Java) applications.</li>
-            <li>Created and maintained Kubernetes deployment configuration files.</li>
-            <li>Examimed web applications for security vulerabilities.</li>
+
+            <li>Created automatically generated documentation for component catalogs and a self-generating React.js Icon package</li>
+            <li>Maintained a fork of create-react-app to automate bootstrapping of new projects</li>
+            <li>Created React components to be consumed by a suite of applications</li>
+            <li>Created CI/CD pipelines for frontend (React/AngularJS) and backend (Java) applications</li>
+            <li>Created CI/CD pipeline for Kubernetes application deployments with Helm</li>
+            <li>Migrated legacy monolithic applicaiton to microservice architecture with Helm and Kubernetes</li>
+            <li>Examimed web applications for security vulerabilities</li>
         </ul>,
     },
     {
@@ -46,10 +48,17 @@ const experienceData = [
             { label: 'Arduino' },
             { label: 'Docker' },
         ],
-        description: <ul style={{ margin: 0, paddingLeft: 20 }}>
-            <li>Worked with clients to develop solutions from idea to final product.</li>
-            <li>Designed several web applications and maintained WordPress blogs.</li>
-        </ul>,
+        description: <React.Fragment>
+            <ul style={{ margin: 0, paddingLeft: 20 }}>
+                <li>Worked with clients to develop solutions from idea to final product</li>
+                <li>Designed WordPress landing pages and custom solutions</li>
+            </ul>
+            Designed several web applications including:
+            <ul style={{ margin: 0, paddingLeft: 20 }}>
+                <li>Custom Twilio IVR with support for agents, queueing, and call distribution</li>
+                <li>Digital Signage platform with Instagram integration and administration portal</li>
+            </ul>
+        </React.Fragment>,
     },
     {
         heading: 'Developer - VoxelCore',
@@ -62,8 +71,8 @@ const experienceData = [
             { label: 'Twisted' },
         ],
         description: <ul style={{ margin: 0, paddingLeft: 20 }}>
-            <li>Assisted with development of a cleanroom implementation of the Minecraft server using Python and Twisted.</li>
-            <li>Contributed to developing automated gameplay analysis tools to prevent unauthorized game modification.</li>
+            <li>Assisted with development of a cleanroom implementation of the Minecraft server using Python and Twisted</li>
+            <li>Contributed to developing automated gameplay analysis tools to prevent unauthorized game modification</li>
         </ul>,
     },
 ];
