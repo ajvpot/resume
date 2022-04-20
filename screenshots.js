5e4161f629533031cb46db1d9edffa5630905be1
Alex Vanderpot <alex@vanderpot.com>
2022-04-20 12:50:00 -0700

    yarn serve

--- screenshots.js
+++ screenshots.js
@@ -7,15 +7,15 @@ const puppeteer = require('puppeteer');
 
     const page = await browser.newPage();
 
-    await page.goto('http://127.0.0.1:3000/resume', {waitUntil: 'networkidle0'});
+    await page.goto('http://127.0.0.1:3000/resume/index.html', {waitUntil: 'networkidle0'});
     await page.screenshot({path: 'screenshot-desktop.png'});
 
     await page.emulate(puppeteer.devices['iPhone X']);
-    await page.goto('http://127.0.0.1:3000/resume', {waitUntil: 'networkidle0'});
+    await page.goto('http://127.0.0.1:3000/resume/index.html', {waitUntil: 'networkidle0'});
     await page.screenshot({path: 'screenshot-mobile.png'});
 
     await page.emulate(puppeteer.devices['iPad Pro landscape']);
-    await page.goto('http://127.0.0.1:3000/resume', {waitUntil: 'networkidle0'});
+    await page.goto('http://127.0.0.1:3000/resume/index.html', {waitUntil: 'networkidle0'});
     await page.screenshot({path: 'screenshot-tablet.png'});
 
     await browser.close();
