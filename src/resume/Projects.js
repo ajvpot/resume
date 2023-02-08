ad74b1fdc74b82479fa8ce397af2ae8ae07d321b
Alex Vanderpot <alex@vanderpot.com>
2023-02-07 22:27:49 -0800

    update projects

--- src/resume/Projects.js
+++ src/resume/Projects.js
@@ -5,15 +5,17 @@ import SectionItem from './util/sectionItem';
 export default function Projects(props) {
     const researchData = [
         {
-            heading: <React.Fragment>lockfileparsergo {props.isPrinting || <a href="https://pkg.go.dev/github.com/ajvpot/lockfileparsergo" rel="nofollow"><img src="https://pkg.go.dev/badge/github.com/ajvpot/lockfileparsergo" alt="PkgGoDev" /></a>}</React.Fragment>,
+            heading: <React.Fragment>lockfileparsergo {props.isPrinting || <a href="https://pkg.go.dev/github.com/ajvpot/lockfileparsergo" rel="nofollow" style={{float: 'right'}}><img src="https://pkg.go.dev/badge/github.com/ajvpot/lockfileparsergo" alt="PkgGoDev" /></a>}</React.Fragment>,
             description: <React.Fragment>
-                Built a Go library that embeds a V8 runtime with a JavaScript build toolchain and polyfills to enable using
+                Go library that embeds a V8 runtime with a JavaScript build toolchain and polyfills to enable using
                 Snyk's lockfile parsing library in Go.
             </React.Fragment>
         },
         {
-            heading: 'Mobile Video Streaming Solution',
-            description: 'Designed hardware and firmware capable of bonding up to three LTE modems to reliably deliver mobile video streams and other telemetry data at up to 300 megabits per second.'
+            heading: <React.Fragment>vanderbot {props.isPrinting || <a href="https://pkg.go.dev/github.com/ajvpot/vanderbot" rel="nofollow" style={{float: 'right'}}><img src="https://pkg.go.dev/badge/github.com/ajvpot/lockfileparsergo" alt="PkgGoDev" /></a>}</React.Fragment>,
+            description: <React.Fragment>
+                Discord utility bot for website analysis, multitrack recording, multitrack local audio output, and more. Add command modules with go.uber.org/fx.
+            </React.Fragment>
         }
     ];
     return (
